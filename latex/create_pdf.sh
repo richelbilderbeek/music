e03291504a9095e3ea33109d00c94985749ad946
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-01 10:52:55 +0200

    Use LaTeX

---
+++ latex/create_pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Create the PDF 'boost_graph_cookbook_1.pdf'
+#
+# It does so by converting the TeX to PDF, 
+# as well as stitching some other PDFs to it
+#
+# Usage:
+#
+#   ./create.sh
+#
+pdflatex music.tex
